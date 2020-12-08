40e189803b6e42d223d1bf2f72477f369ec19684
Gabriel Pereira de Menezes <gabrielpereiram10@gmail.com>
2020-12-07 22:29:14 -0300

    implement truth value function

--- semantics.py
+++ semantics.py
@@ -1,16 +1,27 @@
 """The goal in this module is to define functions associated with the semantics of formulas in propositional logic. """
 
+from typing import Set, Tuple, Union
 
 from formula import *
 from functions import atoms
 
 
-def truth_value(formula, interpretation):
+def truth_value(formula: Formula, interpretation: Set[Tuple[str, bool]]) -> Union[bool, None]:
     """Determines the truth value of a formula in an interpretation (valuation).
     An interpretation may be defined as dictionary. For example, {'p': True, 'q': False}.
     """
-    pass
-    # ======== YOUR CODE HERE ========
+
+    if isinstance(formula, Atom):
+        return formula.get_value(interpretation.copy())
+    if isinstance(formula, Not):
+        return Not(
+            truth_value(formula.inner, interpretation)
+        ).truth_value()
+    if isinstance(formula, BinaryConnective):
+        return type(formula)(
+            truth_value(formula.left, interpretation),
+            truth_value(formula.right, interpretation)
+        ).truth_value()
 
 
 def is_logical_consequence(premises, conclusion):  # function TT-Entails? in the book AIMA.
@@ -37,5 +48,3 @@ def is_satisfiable(formula):
     Otherwise, it returns False."""
     pass
     # ======== YOUR CODE HERE ========
-
-
